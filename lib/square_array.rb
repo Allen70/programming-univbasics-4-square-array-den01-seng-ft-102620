d57ed30df6780274f07ca81c65121783db243aa8
Allen Richard Lea <allenlea2007@gmail.com>
2020-10-05 04:45:50 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+  i = 0
+  new_array = []
+  while array[i] do 
+  squarenum = (array[i] * array[i])
+  new_array.push(squarenum)
+  binding.pry
+ i += 1
+  
 end
+new_array
+end
+
